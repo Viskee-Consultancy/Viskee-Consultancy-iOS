626a8b1f309164127b2945fbfd81a954ccd0e90a
Yaohua Liang <app.developer@aibtglobal.edu.au>
2021-06-08 23:25:11 +0200

    Initial Commit

---
+++ Viskee-Consultancy-iOS/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Viskee-Consultancy-iOS
+//
+//  Created by Yaohua LIANG on 08/06/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Viskee-Consultancy-iOS/Viskee_Consultancy_iOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  Viskee_Consultancy_iOSApp.swift
+//  Viskee-Consultancy-iOS
+//
+//  Created by Yaohua LIANG on 08/06/2021.
+//
+
+import SwiftUI
+
+@main
+struct Viskee_Consultancy_iOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
